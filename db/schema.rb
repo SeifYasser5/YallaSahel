e8225ab68b4e361dd42cb0025b6005dcf11df934
Seif Yasser <seifyasser@icloud.com>
2019-01-15 12:21:16 +0200

    DB update

--- db/schema.rb
+++ db/schema.rb
@@ -36,13 +36,5 @@ ActiveRecord::Schema.define(version: 2019_01_14_072646) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "rentals", primary_key: "url", id: :integer, default: nil, force: :cascade do |t|
-    t.string "destination", limit: 100
-    t.string "ownername", limit: 100
-    t.string "mobilenumber", limit: 20
-    t.integer "rooms"
-    t.integer "bathrooms"
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
