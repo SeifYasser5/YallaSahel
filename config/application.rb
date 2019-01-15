ddc557403ea22660ef2631ce33b0e5fe390cf2fa
Seif Yasser <seifyasser@icloud.com>
2019-01-15 12:03:39 +0200

    DB update

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module YallaSahel
     config.load_defaults 5.2
     #To speed up asset precompiles
     config.assets.initialize_on_precompile = false
-    config.assets.compile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
